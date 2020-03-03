c86e7dadb77bb2014992ff79249d5d13b1a1c369
phatdroid94 <phatdroid94@gmail.com>
2020-03-03 18:23:52 +0700

    1 : setup retrofit

--- app/src/main/java/com/example/retrofit26112019/MainActivity.java
+++ app/src/main/java/com/example/retrofit26112019/MainActivity.java
@@ -4,11 +4,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Protocol;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 1 : Set up retrofit
+        Gson gson = new GsonBuilder()
+                .disableHtmlEscaping()
+                .setLenient()
+                .create();
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                            .readTimeout(300 , TimeUnit.SECONDS)
+                            .writeTimeout(300,TimeUnit.SECONDS)
+                            .connectTimeout(300 , TimeUnit.SECONDS)
+                            .retryOnConnectionFailure(true)
+                            .protocols(Arrays.asList(Protocol.HTTP_1_1))
+                            .build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                            .baseUrl("https://khoapham.vn/KhoaPhamTraining/json/tien/")
+                            .addConverterFactory(GsonConverterFactory.create(gson))
+                            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                            .client(okHttpClient)
+                            .build();
     }
 }
