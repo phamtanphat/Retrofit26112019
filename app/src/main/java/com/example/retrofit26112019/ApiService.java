df343cded10ee72a5ed7b944ce91c1ffc3a3affe
phatdroid94 <phatdroid94@gmail.com>
2020-03-03 18:55:54 +0700

    finish demo 1

--- app/src/main/java/com/example/retrofit26112019/ApiService.java
+++ app/src/main/java/com/example/retrofit26112019/ApiService.java
@@ -1,9 +1,11 @@
 package com.example.retrofit26112019;
 
+import io.reactivex.Observable;
 import retrofit2.http.GET;
 
 public interface ApiService {
     //https://khoapham.vn/KhoaPhamTraining/json/tien/demo1.json
 
     @GET("demo1.json")
+    Observable<Demo1> getDataDemo1();
 }
--- app/src/main/java/com/example/retrofit26112019/MainActivity.java
+++ app/src/main/java/com/example/retrofit26112019/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.retrofit26112019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -11,6 +12,12 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Protocol;
@@ -47,6 +54,33 @@ public class MainActivity extends AppCompatActivity {
                             .build();
 
         // 2 : Khoi tao interface chua cac request
+        ApiService apiService = retrofit.create(ApiService.class);
+
+        //3 : Goi request
+        apiService.getDataDemo1()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Demo1>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Demo1 demo1) {
+                        Log.d("BBB",demo1.getFanpage());
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
 
     }
 }
